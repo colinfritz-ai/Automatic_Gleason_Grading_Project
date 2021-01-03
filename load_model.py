94a9a416a819b72074b732f81ce1615d416f31f5
ColinFritzLtts <colinfritz.ai@gmail.com>
2021-01-03 10:39:45 -0600

    added the min area rect function

--- load_model.py
+++ load_model.py
@@ -5,15 +5,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys 
 
-fashion_mnist = tf.keras.datasets.fashion_mnist
+# fashion_mnist = tf.keras.datasets.fashion_mnist
 
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+# (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
-train_images = train_images / 255.0
-print(train_images[0].shape)
-test_images = test_images / 255.0
-new_model = tf.keras.models.load_model('fashion_mnist')
-predictions=new_model.predict(train_images[0:2])
-print("predictions: " + str(predictions))
-
+# train_images = train_images / 255.0
+# print(train_images[0].shape)
+# test_images = test_images / 255.0
+# new_model = tf.keras.models.load_model('fashion_mnist')
+# predictions=new_model.predict(train_images[0:2])
+# print("predictions: " + str(predictions))
+node = [1,2,3]
+a = node[-1]
+node.append(4)
+print(a)
 
