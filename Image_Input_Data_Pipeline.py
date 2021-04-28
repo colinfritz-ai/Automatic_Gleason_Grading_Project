48804f6e800ab6d567074098cff3784e518a6052
ColinFritzLtts <colinfritz.ai@gmail.com>
2021-04-28 13:17:49 -0500

    TFRecord file formation and reading into tf.Data dataset pipeline tested locally.  Everything works but need to figure out how to get parallel reads of the files to speed up ingestion.

--- Image_Input_Data_Pipeline.py
+++ Image_Input_Data_Pipeline.py
@@ -12,15 +12,9 @@ import matplotlib.pyplot as plt
 import cv2 
  
 tissue_sample_filename = '/Users/colinfritz/Desktop/my_repos/Automatic_Gleason_Grading_Project/images/000920ad0b612851f8e01bcc880d9b3d.tiff'
-tissue_sample = tiff.imread(tissue_sample_filename)
-
-# The array is showing all RGB values as 255.  This is incorrect behavior.  
-#print("image_as_array: " + str(tissue_sample))
-# trying to print the image also does not work in matplotlib despite it being a valid numpy array.  
-#plt.figure()
-#plt.imshow(tissue_sample)
-#plt.show()
-
+tissue_sample = tiff.imread(tissue_sample_filename)  
+plt.imshow(tissue_sample)
+plt.show()
 # pts1 = np.float32([[5770,11800],[7820,13000],[15000,2500],[14000,1500]])
 # pts2 = np.float32([[0,0],[3000,0],[0,12000],[3000,12000]])
 # M = cv2.getPerspectiveTransform(pts1,pts2)
@@ -48,20 +42,20 @@ tissue_sample = tiff.imread(tissue_sample_filename)
 # plt.imshow(tissue_sample[:,:,2])
 # plt.show()
 
-shape = (tissue_sample[:,:,0].shape[1], tissue_sample[:,:,0].shape[0]) # cv2.warpAffine expects shape in (length, height)
+# shape = (tissue_sample[:,:,0].shape[1], tissue_sample[:,:,0].shape[0]) # cv2.warpAffine expects shape in (length, height)
 
-matrix = cv2.getRotationMatrix2D(center=(11000,7900), angle=90, scale=1)
-image1 = cv2.warpAffine( src=tissue_sample[:,:,0], M=matrix, dsize=shape )
-image2 = cv2.warpAffine( src=tissue_sample[:,:,1], M=matrix, dsize=shape )
-image3 = cv2.warpAffine( src=tissue_sample[:,:,2], M=matrix, dsize=shape )
+# matrix = cv2.getRotationMatrix2D(center=(11000,7900), angle=90, scale=1)
+# image1 = cv2.warpAffine( src=tissue_sample[:,:,0], M=matrix, dsize=shape )
+# image2 = cv2.warpAffine( src=tissue_sample[:,:,1], M=matrix, dsize=shape )
+# image3 = cv2.warpAffine( src=tissue_sample[:,:,2], M=matrix, dsize=shape )
 
-final = np.stack((image1,image2,image3), axis =2)
-plt.subplot(121)
-plt.imshow(tissue_sample)
-plt.title('Input')
-plt.subplot(122)
-plt.imshow(final)
-plt.title('Output')
-plt.show()
+# final = np.stack((image1,image2,image3), axis =2)
+# plt.subplot(121)
+# plt.imshow(tissue_sample)
+# plt.title('Input')
+# plt.subplot(122)
+# plt.imshow(final)
+# plt.title('Output')
+# plt.show()
 
 
--- Resize_and_Save.py
+++ Resize_and_Save.py
@@ -28,8 +28,8 @@ class prepare_TFRecords():
 		img_decoded=tfio.experimental.image.decode_tiff(img)
 		rgb_img=img_decoded[:,:,0:3]
 		rgb_img=tf.image.resize(rgb_img, size = new_shape)
-		encoded_jpeg=tf.io.encode_jpeg(tf.cast(rgb_img, dtype=tf.uint8))
-		encoded_jpeg = tf.io.serialize_tensor(encoded_jpeg).numpy()
+		encoded_jpeg=tf.io.encode_jpeg(tf.cast(rgb_img, dtype=tf.uint8)).numpy()
+		#encoded_jpeg = tf.io.serialize_tensor(encoded_jpeg).numpy()
 		return encoded_jpeg
 
 	def ohe_label(self,label_string):
@@ -46,9 +46,24 @@ class prepare_TFRecords():
 		}
 		return tf.train.Example(features=tf.train.Features(feature=feature))
 	
-	def read_TFRecord(observation):
-		return
+	def read_TFRecord(self,observation):
+		features = {
+		"image": tf.io.FixedLenFeature([], tf.string),
+		"label": tf.io.VarLenFeature(tf.float32)
+		}
 
+		observation = tf.io.parse_single_example(observation, features)
+
+		# FixedLenFeature fields are now ready to use: exmple['size']
+		# VarLenFeature fields require additional sparse_to_dense decoding
+
+		image = tf.image.decode_jpeg(observation['image'], channels=3)
+		# image = tf.reshape(image, [*TARGET_SIZE, 3])
+
+		# label  = example['label']
+		label = tf.sparse.to_dense(observation['label'])
+		return (image, label)
+		
 
 preparer=prepare_TFRecords()
 shards = 1
@@ -57,13 +72,15 @@ filename = "/Users/colinfritz/Desktop/my_repos/Automatic_Gleason_Grading_Project
 for shard in range(shards):
 	with tf.io.TFRecordWriter(filename) as out_file:
 	    for i in range(shard_size):
-		    image = preparer.resize_and_encode("/Users/colinfritz/Desktop/my_repos/Automatic_Gleason_Grading_Project/images/004391d48d58b18156f811087cd38abf.tiff")
+		    image = preparer.resize_and_encode("/Users/colinfritz/Desktop/my_repos/Automatic_Gleason_Grading_Project/images/000920ad0b612851f8e01bcc880d9b3d.tiff")
 		    label = preparer.ohe_label("2")
 		    example = preparer.to_TFRecord(out_file,
 		                            image, #already encoded as a byte_string
 		                            label
 		                          )
 		    out_file.write(example.SerializeToString())
+
+
 
 
 
